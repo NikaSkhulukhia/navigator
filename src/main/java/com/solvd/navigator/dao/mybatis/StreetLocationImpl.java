549bb2930e57b6c6d7a69bf0542741866ac9208a
mirian228 <pariksini@mail.ru>
2023-02-22 13:30:54 +0400

    refactored code to fix issues with database logic and loggers

--- src/main/java/com/solvd/navigator/dao/mybatis/StreetLocationImpl.java
+++ src/main/java/com/solvd/navigator/dao/mybatis/StreetLocationImpl.java
@@ -12,77 +12,78 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class StreetLocationImpl implements IStreetLocationDao {
-    private static final Logger LOGGER = LogManager.getLogger(StreetImpl.class);
-    private static final SqlSessionFactory SESSION_FACTORY = MyBatisUtil.getSqlSessionFactory();
-    StreetLocation streetLocation;
+	private static final SqlSessionFactory SESSION_FACTORY = MyBatisUtil.getSqlSessionFactory();
+	private static final Logger LOGGER = LogManager.getLogger(StreetImpl.class);
 
-    @Override
-    public void insertEntity(StreetLocation entity) throws SQLException {
-        try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
-            IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
-            try {
-                iStreetLocationDao.insertEntity(entity);
-                sqlSession.commit();
-            } catch (SQLException e) {
-                LOGGER.error("SQLException", e);
-            } finally {
-                sqlSession.rollback();
-            }
-        }
-    }
+	StreetLocation streetLocation;
 
-    @Override
-    public StreetLocation selectEntityById(int id) throws SQLException {
-        try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
-            IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
-            streetLocation = iStreetLocationDao.selectEntityById(id);
-        } catch (SQLException e) {
-            LOGGER.error("SQLException", e);
-        }
-        return streetLocation;
-    }
+	@Override
+	public void insertEntity(StreetLocation entity) throws SQLException {
+		try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
+			IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
+			try {
+				iStreetLocationDao.insertEntity(entity);
+				sqlSession.commit();
+			} catch (SQLException e) {
+				LOGGER.error("SQLException", e);
+			} finally {
+				sqlSession.rollback();
+			}
+		}
+	}
 
-    @Override
-    public List<StreetLocation> selectAllEntity() throws SQLException {
-        List<StreetLocation> streetLocationList = null;
-        try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
-            try {
-                IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
-                streetLocationList = iStreetLocationDao.selectAllEntity();
-            } catch (SQLException e) {
-                LOGGER.error("SQLException", e);
-            }
-        }
-        return streetLocationList;
-    }
+	@Override
+	public StreetLocation selectEntityById(int id) throws SQLException {
+		try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
+			IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
+			streetLocation = iStreetLocationDao.selectEntityById(id);
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
+		return streetLocation;
+	}
 
-    @Override
-    public void updateEntity(StreetLocation entity) throws SQLException {
-        try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
-            IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
-            try {
-                iStreetLocationDao.updateEntity(entity);
-                sqlSession.commit();
-            } catch (SQLException e) {
-                LOGGER.error("SQLException", e);
-            } finally {
-                sqlSession.rollback();
-            }
-        }
-    }
+	@Override
+	public List<StreetLocation> selectAllEntity() throws SQLException {
+		List<StreetLocation> streetLocationList = null;
+		try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
+			IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
+			try {
+				streetLocationList = iStreetLocationDao.selectAllEntity();
+			} catch (SQLException e) {
+				LOGGER.error("SQLException", e);
+			}
+		}
+		return streetLocationList;
+	}
 
-    @Override
-    public void deleteEntity(int id) throws SQLException {
-        try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
-            IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
-            try {
-                iStreetLocationDao.deleteEntity(id);
-                sqlSession.commit();
-            } catch (SQLException e) {
-                LOGGER.error("SQLException", e);
-            } finally {
-                sqlSession.rollback();
-            }
-        }
-    }
+	@Override
+	public void updateEntity(StreetLocation entity) throws SQLException {
+		try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
+			IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
+			try {
+				iStreetLocationDao.updateEntity(entity);
+				sqlSession.commit();
+			} catch (SQLException e) {
+				LOGGER.error("SQLException", e);
+			} finally {
+				sqlSession.rollback();
+			}
+		}
+	}
+
+	@Override
+	public void deleteEntity(int id) throws SQLException {
+		try (SqlSession sqlSession = SESSION_FACTORY.openSession()) {
+			IStreetLocationDao iStreetLocationDao = sqlSession.getMapper(IStreetLocationDao.class);
+			try {
+				iStreetLocationDao.deleteEntity(id);
+				sqlSession.commit();
+			} catch (SQLException e) {
+				LOGGER.error("SQLException", e);
+			} finally {
+				sqlSession.rollback();
+			}
+		}
+	}
 }
