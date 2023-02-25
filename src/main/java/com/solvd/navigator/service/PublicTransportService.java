e1cc83bf41576b747021edcb2ac09670772ce4de
Nika Skhulukhia <nskhu12@gmail.com>
2023-02-25 21:14:39 +0400

    finish PublicTransportService implementation

--- src/main/java/com/solvd/navigator/service/PublicTransportService.java
+++ src/main/java/com/solvd/navigator/service/PublicTransportService.java
@@ -8,7 +8,6 @@ import com.solvd.navigator.dao.mybatis.PublicTransportDirectionsImpl;
 import com.solvd.navigator.dao.mybatis.PublicTransportImpl;
 import com.solvd.navigator.dao.mybatis.StreetImpl;
 import com.solvd.navigator.dao.mybatis.StreetLocationImpl;
-import com.solvd.navigator.main.Main;
 import com.solvd.navigator.model.PublicTransport;
 import com.solvd.navigator.model.Street;
 import com.solvd.navigator.model.StreetLocation;
@@ -17,78 +16,45 @@ import org.apache.logging.log4j.Logger;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class PublicTransportService {
     private static final Logger LOGGER = LogManager.getLogger(PublicTransportService.class);
-    private List<Integer> pathIds;
-    private Map<Integer, Integer> addressForBusChange;
+    private List<Integer> pathIds; // best path stations ids, needs to be set from floyd service class
 
     public PublicTransportService() {
-    }
-
-    private  String addressForBusChangeToString() throws SQLException {
-        String result = "";
-        IPublicTransportDao publicTransport = new PublicTransportImpl();
-        IStreetLocationDao streetLocation = new StreetLocationImpl();
-        IStreetDao street = new StreetImpl();
-
-        for (Map.Entry<Integer,Integer> entry : addressForBusChange.entrySet()) {
-            StreetLocation addressObj = streetLocation.selectEntityById(entry.getKey());
-            Street addressStreetObj = street.selectEntityById(addressObj.getIdStreet());
-            PublicTransport busObj = publicTransport.selectEntityById(entry.getValue());
-            String addressStr = addressObj.getStreetNumber() + ", " + addressStreetObj.getName();
-            int busNumber = busObj.getPublicTransportNumber();
-            result += "At the station: " + addressStr + " take the bus #: " + busNumber + " ---> ";
-        }
-        return result;
+        pathIds = new ArrayList<>();
     }
 
     public String getBusUsagePlan() throws SQLException {
-        // TODO pathIds must be set to this class object from another class or method.
-        pathIds = new ArrayList<>();
-        pathIds.add(3);
-        pathIds.add(2);
-        pathIds.add(4);
-        pathIds.add(1);
-
         IPublicTransportDirectionsDao publicTransportDirections = new PublicTransportDirectionsImpl();
-        addressForBusChange = new HashMap<>();
+        IPublicTransportDao publicTransport = new PublicTransportImpl();
+        IStreetLocationDao streetLocation = new StreetLocationImpl();
+        IStreetDao street = new StreetImpl();
         String result = "";
-        String res2 = "";
         int currentBusId = 0;
         for(int i = 0; i < pathIds.size() - 1; i++) {
             int currentStationId = pathIds.get(i);
-            res2 += "    ----   " + currentStationId + "    ----    ";
             int nextStationId = pathIds.get(i+1);
             List<Integer> currentStationBusIds = publicTransportDirections.selectAllBusIdsByLocationId(currentStationId);
             List<Integer> nextStationBusIds = publicTransportDirections.selectAllBusIdsByLocationId(nextStationId);
-            if (currentBusId != 0 && currentStationBusIds.contains(currentBusId)
-                    && nextStationBusIds.contains(currentBusId)) {
-                // თუ მიმდინარე ბასი გვაქვს
-                // შემოწმდეს ეს ბასი თუ არის მიმდინარე ადრესის ბასებში და ასევე შემდეგი ადრესის ბასებში.
-                // თუ არის ორივეში გადავიდეთ შემდეგ ციკლზე, CONTINUE
+            if (currentBusId != 0 && currentStationBusIds.contains(currentBusId) && nextStationBusIds.contains(currentBusId)) {
                 continue;
             }
 
             for (int currentStationBusId : currentStationBusIds) {
-                // გადავყვეთ მიმდინარე ადრესის ბასებს
-                    // ამოვიღოთ ბასი
-                    // შევამოწმოთ ეს ბასი თუ არის შემდეგი ადრესის ბასებში
-                    // თუ არის
-                        // მიმდინარე ბასი გახდეს ეს ბასი
-                        // რესალტში ჩავწეროთ მიმდინარე ადრესსზე curentStationId ჩაჯექით ბასში 'მიმდინარე ბასი'
                 if (nextStationBusIds.contains(currentStationBusId)) {
                     currentBusId = currentStationBusId;
-                    addressForBusChange.put(currentStationId, currentBusId);
-                    //result += addressForBusChangeToString();
-                    result += "At the station: " + currentStationId + " take the bus: " + currentBusId + " ---> ";
+                    StreetLocation addressObj = streetLocation.selectEntityById(currentStationId);
+                    Street addressStreetObj = street.selectEntityById(addressObj.getIdStreet());
+                    PublicTransport busObj = publicTransport.selectEntityById(currentBusId);
+                    String addressStr = addressObj.getStreetNumber() + ", " + addressStreetObj.getName();
+                    int busNumber = busObj.getPublicTransportNumber();
+                    result += "At the station: " + addressStr + " take the bus #: " + busNumber + " ---> ";
+                    break;
                 }
             }
         }
-        LOGGER.info(" current station ids: " + res2);
         return result;
     }
 
